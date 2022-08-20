49b21e594edc4a690b17f2b7b9e99bb9d7ae38a2
Ryan Dines <ryandines@Ryans-MacBook-Pro.local>
2022-08-20 18:14:02 -0400

    Trying server url.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : process.env.VERCEL_URL;
+export const server = dev ? 'http://localhost:3000' : 'https://biz-app-roan.vercel.app';
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
 
   const res = await fetch(`${server}/api/page-data`);
   const pageData = await res.json();
